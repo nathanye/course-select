0d91ea4da27a71f1916f6973e78e79ed5794a76f
nathanye <khalil54781@gmail.com>
2017-09-07 21:00:33 +0800

    Add course and user model

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170907124416) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "course_code"
+    t.string   "course_type"
+    t.string   "teaching_type"
+    t.string   "exam_type"
+    t.string   "credit"
+    t.integer  "limit_num"
+    t.integer  "student_num",   default: 0
+    t.string   "class_room"
+    t.string   "course_time"
+    t.string   "course_week"
+    t.integer  "teacher_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["teacher_id"], name: "index_courses_on_teacher_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "num"
+    t.string   "major"
+    t.string   "department"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",           default: false
+    t.boolean  "teacher",         default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+end
